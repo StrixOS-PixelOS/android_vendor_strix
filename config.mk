ad73e5a88b15855f82e9c530398943240b60bd5f
strix <android@strixos.org>
2026-09-24 11:03:21 +0000

    Rename lineage to avoid detection

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
-include vendor/custom/prebuilts/prebuilts.mk
+include vendor/strix/prebuilts/prebuilts.mk
 RELAX_USES_LIBRARY_CHECK=true
 
 # Debloater
--- prebuilts/prebuilts.mk
+++ prebuilts/prebuilts.mk
@@ -3,7 +3,3 @@ LOCAL_CHECK_ELF_FILES := false
 #PRODUCT_PACKAGES +=\
 #    wg \
 #    wg-quick
-
-# Fonts
-#PRODUCT_COPY_FILES += \
-#    $(call find-copy-subdir-files,*,vendor/custom/prebuilts/product/fonts/,$(TARGET_COPY_OUT_PRODUCT)/fonts)
