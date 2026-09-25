1c2fa6411cb4a6fce6c85e3835c87e9ae7c4885c
strix <android@strixos.org>
2026-09-25 23:43:01 +0000

    Rename lineage to avoid detection

--- debloater/Android.mk
+++ debloater/Android.mk
@@ -5,6 +5,8 @@ LOCAL_MODULE := Debloater
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES += \
+    nano \
+    vim \
     EuiccGoogle \
     EasterEgg \
     MarkupGoogle_v2 \
